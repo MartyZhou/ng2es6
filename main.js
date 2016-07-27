33bfda236c528c9f0ede16f1d6bb90164726fd22
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-07-27 14:58:25 +0800

    init commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import 'es6-shim';
+import 'reflect-metadata';
+import 'zone.js/dist/zone-node';
+
+import { bootstrap } from 'angular2/platform/browser';
+import { AppComponent } from './app/app.component';
+
+bootstrap(AppComponent);
